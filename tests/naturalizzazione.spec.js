4d56519fe4f101bda982612160e92b375837a88c
Matteo Piazza <matteo.piazza@msd.com>
2023-09-15 17:17:03 +0200

    real test

---
+++ tests/naturalizzazione.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+const url = 'https://www.ifc.ti.ch/corsi/tutti-i-corsi/dettaglio/2324asoc0140001_corsi-di-formazione-alla-cittadinanza';
+
+test('Le iscrizioni al corso di naturalizzazione sono aperte?', async ({ page }) => {
+  await page.goto(url);
+  const btnIscriviti = await page.getByRole('button', { name: /iscriviti/i });
+  await expect(btnIscriviti).toHaveClass(/disabled/);
+});
